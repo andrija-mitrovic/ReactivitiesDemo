3c50b0d532acdd5aa078dee56735f0f799471fd3
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-04 10:15:11 +0200

    Configure database

---
+++ src/Infrastructure/Persistence/Configurations/ActivityConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class ActivityConfiguration : IEntityTypeConfiguration<Activity>
+    {
+        public void Configure(EntityTypeBuilder<Activity> builder)
+        {
+            builder.Property(x => x.Category)
+                   .HasMaxLength(100)
+                   .IsRequired();
+
+            builder.Property(x => x.City)
+                   .HasMaxLength(100)
+                   .IsRequired();
+
+            builder.Property(x => x.Title)
+                   .HasMaxLength(100)
+                   .IsRequired();
+
+            builder.Property(x => x.Venue)
+                   .HasMaxLength(100)
+                   .IsRequired();
+            
+            builder.Property(x => x.Description)
+                   .HasMaxLength(200)
+                   .IsRequired();
+        }
+    }
+}
