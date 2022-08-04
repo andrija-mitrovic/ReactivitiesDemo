8e79f466310968f500436a40a07560c491bc0124
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-04 10:14:38 +0200

    Add application context

---
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Infrastructure.Persistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Activity> Activities => Set<Activity>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
