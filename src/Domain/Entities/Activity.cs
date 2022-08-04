dac8f938e67af7ce48851ab1ee62a29b4fe56fb1
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-04 10:14:13 +0200

    Add activity entity

---
+++ src/Domain/Entities/Activity.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Activity : BaseEntity
+    {
+        public string? Title { get; set; }
+        public DateTime Date { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public string? City { get; set; }
+        public string? Venue { get; set; }
+    }
+}
