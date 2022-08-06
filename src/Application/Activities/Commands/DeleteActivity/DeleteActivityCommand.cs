c7f88c8eb23c0d12e878b3b3972af26801b01045
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-06 08:35:56 +0200

    Add delete activity command feature

---
+++ src/Application/Activities/Commands/DeleteActivity/DeleteActivityCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Application.Activities.Commands.DeleteActivity
+{
+    public record DeleteActivityCommand(int Id) : IRequest { }
+}
---
+++ src/Application/Activities/Commands/DeleteActivity/DeleteActivityCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Helpers;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Activities.Commands.DeleteActivity
+{
+    public class DeleteActivityCommandHandler : IRequestHandler<DeleteActivityCommand>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly ILogger<DeleteActivityCommandHandler> _logger;
+
+        public DeleteActivityCommandHandler(
+            IApplicationDbContext context,
+            ILogger<DeleteActivityCommandHandler> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(DeleteActivityCommand request, CancellationToken cancellationToken)
+        {
+            var activity = await _context.Activities.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (activity == null)
+            {
+                _logger.LogError(HelperFunction.GetMethodName() + " - Activity with Id: {ActivityId} not found", activity!.Id);
+                throw new NotFoundException(nameof(Activity), request.Id);
+            }
+
+            _context.Activities.Remove(activity!);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            _logger.LogInformation("Activity {ActivityId} is successfully deleted.", activity.Id);
+
+            return Unit.Value;
+        }
+    }
+}
