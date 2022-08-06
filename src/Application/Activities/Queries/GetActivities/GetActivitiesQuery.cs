7885c40425de4257400030b654c8c7c2af91f58d
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-06 08:36:30 +0200

    Add get activities query feature

---
+++ src/Application/Activities/Queries/GetActivities/GetActivitiesQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.Common.DTOs;
+using MediatR;
+
+namespace Application.Activities.Queries.GetActivities
+{
+    public class GetActivitiesQuery : IRequest<List<ActivityDto>>
+    {
+    }
+}
---
+++ src/Application/Activities/Queries/GetActivities/GetActivitiesQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Common.DTOs;
+using Application.Common.Interfaces;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Activities.Queries.GetActivities
+{
+    public class GetActivitiesQueryHandler : IRequestHandler<GetActivitiesQuery, List<ActivityDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetActivitiesQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ActivityDto>> Handle(GetActivitiesQuery request, CancellationToken cancellationToken)
+        {
+            var activities = await _context.Activities.AsNoTracking().ToListAsync();
+
+            return _mapper.Map<List<ActivityDto>>(activities);
+        }
+    }
+}
