f25c2fda8c89e0ac3892927f4c48e70515cb6ada
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-07 08:50:52 +0200

    Add mapping profile

---
+++ src/Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using Application.Activities.Commands.CreateActivity;
+using Application.Activities.Commands.UpdateActivity;
+using Application.Common.DTOs;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Activity, CreateActivityCommand>().ReverseMap();
+            CreateMap<Activity, UpdateActivityCommand>().ReverseMap();
+            CreateMap<Activity, ActivityDto>().ReverseMap();
+        }
+    }
+}
