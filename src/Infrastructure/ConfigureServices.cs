9e49cdb6572448e46650026fa8b1f1560695d58d
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-07 08:50:39 +0200

    Configure infrastructure project

---
+++ src/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Interceptors;
+using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ConfigureServices
+    {
+        private const string CONNECTION_STRING_NAME = "DefaultConnection";
+
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString(CONNECTION_STRING_NAME),
+                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+            services.AddScoped<ApplicationDbContextInitialiser>();
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            services.AddTransient<IDateTime, DateTimeService>();
+
+            return services;
+        }
+    }
+}
