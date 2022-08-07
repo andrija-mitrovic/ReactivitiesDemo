4e89086c11979d00e896253d032afbe557adb248
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-08-07 08:51:44 +0200

    Configure api project

---
+++ src/API/ConfigureServices.cs
@@ -0,0 +1,30 @@
+﻿using API.Services;
+using Application.Common.Interfaces;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddAPIServices(this IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+
+            services.AddSingleton<ICurrentUserService, CurrentUserService>();
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
